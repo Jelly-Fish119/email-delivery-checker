3b92cefd1a0189b3597b7268e47801b938170045
Jelly-Fish119 <skmoinuddin0800@gmail.com>
2025-06-18 16:15:18 +0300

    10th commit

--- email_checker_pro/settings.py
+++ email_checker_pro/settings.py
@@ -80,7 +80,7 @@ ROOT_URLCONF = 'email_checker_pro.urls'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    #  'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': 'email_checker_db',
