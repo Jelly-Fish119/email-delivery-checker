ca579b6bff7defbbd190382b313ea708c00b61a0
Jelly-Fish119 <skmoinuddin0800@gmail.com>
2025-06-25 00:46:00 +0300

    update the apps.py in dashboard 2nd

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -2,7 +2,6 @@
 
 from django.apps import AppConfig
 import threading
-from django.db.models.signals import post_migrate
 
 
 class DashboardConfig(AppConfig):
@@ -10,13 +9,11 @@ class DashboardConfig(AppConfig):
     name = 'dashboard'
 
     def ready(self):
-        from django.db import connection
-        if not connection.introspection.table_names():
-            return
+        def start_if_tables_exist():
+            from django.db import connection
+            if not connection.introspection.table_names():
+                return
+            from .mail_checker import start_realtime_listeners
+            start_realtime_listeners()
 
-        # Use post_migrate signal to ensure database is ready
-        post_migrate.connect(self._start_listeners, sender=self)
-
-    def _start_listeners(self, sender, **kwargs):
-        from .mail_checker import start_realtime_listeners
-        threading.Thread(target=start_realtime_listeners, daemon=True).start()
+        threading.Thread(target=start_if_tables_exist, daemon=True).start()
