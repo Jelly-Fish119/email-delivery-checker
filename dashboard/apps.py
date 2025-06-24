f3768775bfe6288725d8bf992fc7f899c0e90a34
Jelly-Fish119 <skmoinuddin0800@gmail.com>
2025-06-24 23:57:01 +0300

    add post_migrate app.py in dashboard

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -2,6 +2,7 @@
 
 from django.apps import AppConfig
 import threading
+from django.db.models.signals import post_migrate
 
 
 class DashboardConfig(AppConfig):
@@ -13,6 +14,9 @@ class DashboardConfig(AppConfig):
         if not connection.introspection.table_names():
             return
 
-        from .mail_checker import start_realtime_listeners
+        # Use post_migrate signal to ensure database is ready
+        post_migrate.connect(self._start_listeners, sender=self)
 
+    def _start_listeners(self, sender, **kwargs):
+        from .mail_checker import start_realtime_listeners
         threading.Thread(target=start_realtime_listeners, daemon=True).start()
